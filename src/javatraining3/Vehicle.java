99cf11fb850bde9d1212a7258fa06a38bedd1bdb
KingGugu <dangyizhou@vip.qq.com>
2020-07-09 11:54:54 +0800

    123

---
+++ src/javatraining3/Vehicle.java
@@ -0,0 +1,43 @@
+package javatraining3;
+
+public class Vehicle {
+	
+	private String brand;
+	private String color;
+	private double speed;
+
+	public Vehicle(String brand,String color) {
+		this.brand=brand;
+		this.color=color;
+		this.speed=0;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public double getSpeed() {
+		return speed;
+	}
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	
+	public void run() {
+		System.out.print(this.brand+"车在跑");
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+	}
+
+}
---
+++ src/javatraining3/VehicleTest.java
@@ -0,0 +1,11 @@
+package javatraining3;
+
+public class VehicleTest {
+
+	public static void main(String[] args) {
+		Vehicle car = new Vehicle("benz","black");
+		car.run();
+
+	}
+
+}
